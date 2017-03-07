8c585a5a492618f24cd6378623ef31bb2d45c5b8
Drew Hart <drew@drewhart.com>
2017-03-07 16:30:23 -0600

    added some example routes

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-  console.log("home page hit...");
   res.send("Hi there");
 });
 
-app.listen(3000, function(){
+app.get("/r/:subredditName", function(req, res){
+  console.log(req.params);
+  res.send("WELCOME SO SUBREDDIT");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+  console.log(req.params);
+  res.send("WELCOME SO SUBREDDIT");
+});
+
+app.get("*", function(req, res) { // route order matters
+  res.send("GENERIC PAGE");
+});
+
+app.listen(8080, function(){
   console.log("Server is running");
 });
